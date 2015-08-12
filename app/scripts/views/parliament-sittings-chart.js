9ccf1c428413db70ebcdef5734e0eba525dc0cb5
Benjamin C <zxzxlch@gmail.com>
2015-08-13 01:10:29 +0800

    Add introduction text and chart guide

--- app/scripts/views/parliament-sittings-chart.js
+++ app/scripts/views/parliament-sittings-chart.js
@@ -12,7 +12,31 @@ let totalSittings = 115;
 module.exports = Backbone.View.extend({
 
   initialize: function(options) {
-    data = _.sortBy(data, 'spoken').reverse();
+    this.initChartGuide();
+    this.initRows();
+  },
+
+
+  initChartGuide: function() {
+    // Chart guide
+    let $chartGuide = $('.chart-guide');
+
+    let attendencePercent = 80;
+    let spokenPercent = 40;
+
+    $chartGuide.find('.bar-filled').css('width', `${ attendencePercent }%`);
+    $chartGuide.find('.tip-attendence').css('width', `${ attendencePercent }%`);
+
+    $chartGuide.find('.bar-icon-filled').css('width', `${ spokenPercent }%`);
+    $chartGuide.find('.tip-spoken').css('width', `${ spokenPercent }%`);
+
+    $chartGuide.find('.marker-1, .marker-line-1, .tip-mean-attendence').css('left', `${ meanAttendencePercent }%`);
+    $chartGuide.find('.marker-2, .marker-line-2, .tip-mean-spoken').css('left', `${ meanSpokenPercent }%`);
+  },
+
+
+  initRows: function() {
+    data = _.sortBy(data, 'attendence').reverse();
     let $rows = _.map(data, (d) => {
       let $row = $(rowTemplate({ data: d, totalSittings: totalSittings }));
       let attendencePercent = d.attendence / totalSittings * 100;
