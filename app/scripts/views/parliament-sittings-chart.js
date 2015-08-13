40d0e5ff5503f3911e130518d0b7a20edb5b6a84
Benjamin C <zxzxlch@gmail.com>
2015-08-13 12:21:40 +0800

    Fix chart guide layout

--- app/scripts/views/parliament-sittings-chart.js
+++ app/scripts/views/parliament-sittings-chart.js
@@ -12,35 +12,35 @@ let totalSittings = 115;
 module.exports = Backbone.View.extend({
 
   initialize: function(options) {
+    // Calculate aggregate stats
+    this.meanAttendence = d3.mean(data, (d) => d.attendence);
+    this.meanSpoken = d3.mean(data, (d) => d.spoken);
+    this.meanAttendencePercent = this.meanAttendence / totalSittings * 100;
+    this.meanSpokenPercent = this.meanSpoken / totalSittings * 100;
+
     this.initChartGuide();
     this.initRows();
   },
 
 
   initChartGuide: function() {
-    // Calculate aggregate stats
-    let meanAttendence = d3.mean(data, (d) => d.attendence);
-    let meanSpoken = d3.mean(data, (d) => d.spoken);
 
     // Chart guide
     let $chartGuide = $('.chart-guide');
 
     let attendencePercent = 80;
     let spokenPercent = 40;
-    let meanAttendencePercent = meanAttendence / totalSittings * 100;
-    let meanSpokenPercent = meanSpoken / totalSittings * 100;
 
-    $chartGuide.find('.bar-filled').css('width', `${ attendencePercent }%`);
-    $chartGuide.find('.tip-attendence').css('width', `${ attendencePercent }%`);
+    $chartGuide.find('.bar-filled, .tip-attendence').css('width', `${ attendencePercent }%`);
+    $chartGuide.find('.bar-icon-filled, .tip-spoken').css('width', `${ spokenPercent }%`);
 
-    $chartGuide.find('.bar-icon-filled').css('width', `${ spokenPercent }%`);
-    $chartGuide.find('.tip-spoken').css('width', `${ spokenPercent }%`);
-
-    $chartGuide.find('.marker-1, .marker-line-1, .tip-mean-attendence').css('left', `${ meanAttendencePercent }%`);
-    $chartGuide.find('.marker-2, .marker-line-2, .tip-mean-spoken').css('left', `${ meanSpokenPercent }%`);
-    $chartGuide.find('.js-stat-mean-attendence-rate').html(meanSpokenPercent.toFixed(1));
-    $chartGuide.find('.js-stat-mean-attendence-count').html(Math.round(meanAttendence));
-    $chartGuide.find('.js-stat-mean-spoken-count').html(Math.round(meanSpoken));
+    $chartGuide.find('.marker-1').css('left', `${ this.meanAttendencePercent }%`);
+    $chartGuide.find('.marker-2').css('left', `${ this.meanSpokenPercent }%`);
+    $chartGuide.find('.marker-line-1').css('margin-left', `${ this.meanAttendencePercent }%`);
+    $chartGuide.find('.marker-line-2').css('margin-left', `${ this.meanSpokenPercent }%`);
+    $chartGuide.find('.js-stat-mean-attendence-rate').html(this.meanAttendencePercent.toFixed(1));
+    $chartGuide.find('.js-stat-mean-attendence-count').html(Math.round(this.meanAttendence));
+    $chartGuide.find('.js-stat-mean-spoken-count').html(Math.round(this.meanSpoken));
   },
 
 
@@ -53,6 +53,9 @@ module.exports = Backbone.View.extend({
       let $row = $(rowTemplate({ data: d, attendenceRate: Math.round(attendencePercent * 10) / 10 }));
       $row.find('.bar-filled').css('width', `${ attendencePercent }%`);
       $row.find('.bar-icon-filled').css('width', `${ spokenPercent }%`);
+      $row.find('.marker-1').css('left', `${ this.meanAttendencePercent }%`);
+      $row.find('.marker-2').css('left', `${ this.meanSpokenPercent }%`);
+
       return $row;
     });
     $('.chart-content').html($rows);
