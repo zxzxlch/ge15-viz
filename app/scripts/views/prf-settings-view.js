0c874772b6b072c67d1f680752e94a295031cb51
Benjamin C <zxzxlch@gmail.com>
2015-09-09 19:06:57 +0800

    Fix and refactor settings view

--- app/scripts/views/prf-settings-view.js
+++ app/scripts/views/prf-settings-view.js
@@ -4,33 +4,101 @@ let $        = require('jquery'),
     _        = require('lodash'),
     Backbone = require('backbone'),
     router   = require('../routers/prf-router'),
-    PerspectiveSettingsView = require('../views/prf-perspective-settings'),
-    DetailSettingsView = require('../views/prf-detail-settings');
+    perspectiveTemplate = _.template(require('../templates/prf-perspective-settings.html')),
+    detailTemplate = _.template(require('../templates/prf-detail-settings.html'));
 
 module.exports = Backbone.View.extend({
 
   className: 'viz-settings',
 
-  initialize: function(options) {
-    this.perspectiveSettingsView = new PerspectiveSettingsView();
-    this.detailSettingsView = new DetailSettingsView();
-    this.listenTo(this.perspectiveSettingsView, 'search', () => {
-      this.trigger.apply(arguments);
-    });
+  events: {
+    'click .viz-settings-sort a':         'setSort',
+    'click .viz-settings-view a':         'setView',
+    'keyup .viz-settings-search input':   'search',
+    'change .viz-settings-search input':  'search'
   },
 
   updateSettings: function (options) {
     switch (router.perspective) {
       case 'faces':
       case 'parties':
-        this.currentSettingsView = this.perspectiveSettingsView;
+        this.renderPerspectiveSettings();
         break;
       case 'candidates':
-        this.currentSettingsView = this.detailSettingsView;
+        this.renderDetailSettings(options);
         break;
     }
-    this.currentSettingsView.updateSettings(options);
-    this.$el.html(this.currentSettingsView.el);
+  },
+
+  renderPerspectiveSettings: function () {
+    // Faces
+    if (router.perspective == 'faces') {
+      this.$el.html(perspectiveTemplate({
+        view: false,
+        search: true,
+        sort: [{
+          value: 'name',
+          label: 'A to Z'
+        }, {
+          label: 'Party'
+        }]
+      }));
+      
+      var sortValue = router.query.sort || 'name';
+      this.setActive('sort', sortValue);
+    } 
+    // Parties
+    else if (router.perspective == 'parties') {
+      this.$el.html(perspectiveTemplate({
+        search: router.query.view != 'tweets',
+        sort: false,
+        view: [{
+          label: 'Teams'
+        }, {
+          label: 'Tweets'
+        }]
+      }));
+
+      this.setActive('view', router.query.view);
+    }
+
+    this.setActive('perspective', router.perspective);
+  },
+
+  setActive: function (group, value) {
+    _.each(this.$(`.viz-settings-${group} .settings-link`), elem => {
+      $(elem).toggleClass('active', $(elem).data('value') == value);
+    });
+  },
+
+  /**
+   * @param options.back.path
+   * @param options.back.label
+   * @param options.left.path
+   * @param options.left.label
+   * @param options.right.path
+   * @param options.right.label
+   */
+  renderDetailSettings: function (options) {
+    _.defaults(options, {
+      left: false,
+      right: false
+    });
+    this.$el.html(detailTemplate(options));
+  },
+
+  setView: function (event) {
+    event.preventDefault();
+    router.setQueryValue('view', $(event.currentTarget).data('value'));
+  },
+
+  setSort: function (event) {
+    event.preventDefault();
+    router.setQueryValue('sort', $(event.currentTarget).data('value'));
+  },
+
+  search: function (event) {
+    this.trigger('search', $(event.currentTarget).val());
   }
 
 });
